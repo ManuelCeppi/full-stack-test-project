f051b9269b91d80651c5ce8925e7698b58557f8f
Manuel Ceppi <manuel.ceppi@outlook.it>
2024-10-25 09:17:38 +0200

    stack initialization + docker local context

---
+++ docker/api/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# set exit if 
+set -e
+
+# aenmod rewrite to enable url rewriting
+a2enmod rewrite
+
+echo "Composer install - Api"
+# installing all the required packages through composer
+composer install -d /var/www/html
+chmod -R 775 /var/www/html/storage
+chmod -R 775 /var/www/html/bootstrap
+
+php artisan route:cache
+
+# start the apache server and keep it running as a foreground service
+apache2-foreground
